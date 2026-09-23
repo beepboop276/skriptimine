f246a8cd5eaeb387ecbdfea5d0f19e5ec2ec4a60
kertu puna <kertu.puna@voco.ee>
2026-09-23 10:57:26 +0300

    automaatne tervitamine süsteemi kellaaja ja kasutajanimega

---
+++ praks5/yl4.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Skript tervitab sisselogitud kasutajat vastavalt süsteemi kellaajale
+
+# Loeme süsteemist praeguse tunni (00-23)
+aeg=$(date +%H)
+
+# Saame teada praeguse kasutaja nime
+kasutaja=$USER
+
+# Kontrollime kellaaega ja väljastame sobiva tervituse
+if [ $aeg -ge 6 -a $aeg -lt 12 ]; then
+    echo "Tere hommikust, $kasutaja!"
+elif [ $aeg -ge 12 -a $aeg -lt 18 ]; then
+    echo "Tere päevast, $kasutaja!"
+elif [ $aeg -ge 18 -a $aeg -lt 22 ]; then
+    echo "Tere õhtust, $kasutaja!"
+elif [ $aeg -ge 22 -a $aeg -lt 24 ] || [ $aeg -ge 0 -a $aeg -lt 6 ]; then
+    echo "Head ööd, $kasutaja!"
+else
+    echo "Süsteemi kellaaja lugemisel tekkis viga"
+fi
